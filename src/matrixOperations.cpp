ba447eb37fb3b0f5e103674225c5cfe62004f557
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-11 16:58:28 -0500

    overload == operator

--- src/matrixOperations.cpp
+++ src/matrixOperations.cpp
@@ -98,5 +98,20 @@ namespace sclMath {
     mCopy.scale(s);
     return mCopy;
   }
+  //TODO: write a test for this function
+  std::int64_t operator== (const matrix& m1, const matrix& m2){
+    if (m1.getRows()!=m2.getRows()) return false;
+    if (m1.getCols()!=m2.getCols()) return false;
+
+    const std::size_t rows = m1.getRows();
+    const std::size_t cols = m1.getCols();
+
+    for (std::size_t i= 0; i<rows; i++){
+      for( std::size_t j = 0; j<cols; j++){
+        if(m1.get(i,j)!=m2.get(i,j))return false;
+      }
+    }
+    return true;
+  }
 
 }
