be8c0e3ea7bcb79c82210697997b8e04fe37f082
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-12 16:50:08 -0500

    problem 2.1.2

--- examples/examplesFromQuantumComputingBook/2.1.2/src/main.cpp
+++ examples/examplesFromQuantumComputingBook/2.1.2/src/main.cpp
@@ -1,29 +1,64 @@
 /*
- * problem adding vectors
+ * prove the associativity property
+ * brute force approch, not formal niether complete
  */
+#include <cstdlib>
 #include <iostream>
 #include <matrix.h>
-#include <matrixOperations.h>
+#include "matrix.h"
+#include "matrixOperations.h"
+#include <random>
 
+const unsigned int randomSeed = 7483710;
+const std::size_t repetitions = 1000;
+sclMath::ComplexScalar randomComplex()
+{
+    static std::default_random_engine e;
+    static std::uniform_real_distribution<> dis(0, 1); // rage 0 - 1
+    return {dis(e),dis(e)};
+}
 int main(){
+
+  std::srand(randomSeed); 
 
   sclMath::matrix m1 (4,1);
   sclMath::matrix m2 (4,1);
+  sclMath::matrix m3 (4,1);
 
-  // set each value
+  for (std::size_t i = 0; i < repetitions; i++){
+    //fill the matrices with radnom values
+    m1.set(0,0,randomComplex());
+    m1.set(1,0,randomComplex());
+    m1.set(2,0,randomComplex());
+    m1.set(3,0,randomComplex());
 
-  m1.set(0,0,sclMath::ComplexScalar(5,13));
-  m1.set(1,0,sclMath::ComplexScalar(6,2));
-  m1.set(2,0,sclMath::ComplexScalar(0.53,-6));
-  m1.set(3,0,sclMath::ComplexScalar(12));
+    m2.set(0,0,randomComplex());
+    m2.set(1,0,randomComplex());
+    m2.set(2,0,randomComplex());
+    m2.set(3,0,randomComplex());
 
-  m2.set(0,0,sclMath::ComplexScalar(7,-8));
-  m2.set(1,0,sclMath::ComplexScalar(0,-4));
-  m2.set(2,0,sclMath::ComplexScalar(2));
-  m2.set(3,0,sclMath::ComplexScalar(9.4,3));
+    m3.set(0,0,randomComplex());
+    m3.set(1,0,randomComplex());
+    m3.set(2,0,randomComplex());
+    m3.set(3,0,randomComplex());
 
-  std::cout<<m1+m2;
+    sclMath::matrix result1 = (m1+m2)+m3;
+    sclMath::matrix result2 = m1+(m2+m3);
+
+    std::cout<<result1;
+    std::cout<<result2;
+
+  //   if (!(result1 == result2)){
+  //     std::cout<<"FALSE";
+  //     return 1;
+  //   }
+  //
+  }
+  // return 1;
+  //
   
-  //TODO: easier api to initialize matrix
-  //TODO: better print formatting 
+  //TODO:[creation] easier api to initialize matrix
+  //TODO:[creation] better print formatting 
+  //TODO:[creation] possible random matrice intitlzation  my be useefull
+  //TODO:[comparison] overload != operator
 }
