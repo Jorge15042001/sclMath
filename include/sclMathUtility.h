5341b02c34ec68486459a6339a9fc2014927f3bb
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-12 23:33:02 -0500

    added safe floating point comparison

---
+++ include/sclMathUtility.h
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+#include <algorithm>
+#include "matrix.h"
+
+namespace sclMathUtility {
+  bool areaAlmostEqual(sclMath::ComplexScalar a, sclMath::ComplexScalar b);
+  bool areaAlmostEqual(sclMath::Scalar a, sclMath::Scalar b);
+}
---
+++ tests/tests_src/test_almostEqual.cpp
@@ -0,0 +1,62 @@
+#include <catch2/catch_test_macros.hpp>
+#include  "matrix.h"
+#include  "matrixOperations.h"
+#include "sclMathUtility.h"
+
+#include <cmath>
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <iostream>
+
+TEST_CASE("real numbers","[AlmostEqual]"){
+  const sclMath::Scalar a =1;
+  const sclMath::Scalar almost_a = a/std::sqrt(2)*std::sqrt(2);
+
+  const sclMath::Scalar b = 2;
+  const sclMath::Scalar almost_b = 2 / std::sqrt(3) * std::sqrt(3);
+
+  const sclMath::Scalar c = 1;
+  const sclMath::Scalar almost_c = 1 / std::sqrt(7) * std::sqrt(7);
+
+  const sclMath::Scalar d = 0.2;
+  const sclMath::Scalar almost_d = 1 / std::sqrt(5) / std::sqrt(5);
+
+
+
+  REQUIRE(sclMathUtility::areaAlmostEqual(a,almost_a));
+  REQUIRE(sclMathUtility::areaAlmostEqual(b,almost_b));
+  REQUIRE(sclMathUtility::areaAlmostEqual(c,almost_c));
+  REQUIRE(sclMathUtility::areaAlmostEqual(d,almost_d));
+}
+TEST_CASE("complex numbers","[AlmostEqual]"){
+  const sclMath::Scalar a =1;
+  const sclMath::Scalar almost_a = a/std::sqrt(2)*std::sqrt(2);
+
+  const sclMath::Scalar b = 2;
+  const sclMath::Scalar almost_b = 2 / std::sqrt(3) * std::sqrt(3);
+
+  const sclMath::Scalar c = 1;
+  const sclMath::Scalar almost_c = 1 / std::sqrt(7) * std::sqrt(7);
+
+  const sclMath::Scalar d = 0.2;
+  const sclMath::Scalar almost_d = 1 / std::sqrt(5) / std::sqrt(5);
+
+  const sclMath::ComplexScalar ac(a,a);
+  const sclMath::ComplexScalar almost_ac(almost_a,almost_a);
+
+  const sclMath::ComplexScalar bc(b,b);
+  const sclMath::ComplexScalar almost_bc(almost_b,almost_b);
+
+  const sclMath::ComplexScalar cc(c,c);
+  const sclMath::ComplexScalar almost_cc(almost_c,almost_c);
+
+  const sclMath::ComplexScalar dc(d,d);
+  const sclMath::ComplexScalar almost_dc(almost_d,almost_d);
+
+
+  REQUIRE(sclMathUtility::areaAlmostEqual(ac,almost_ac));
+  REQUIRE(sclMathUtility::areaAlmostEqual(bc,almost_bc));
+  REQUIRE(sclMathUtility::areaAlmostEqual(cc,almost_cc));
+  REQUIRE(sclMathUtility::areaAlmostEqual(dc,almost_dc));
+}
