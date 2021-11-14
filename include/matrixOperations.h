b9caa5e6eee9d0fcbbd27b1d43eb7d68e1ddecf5
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-14 02:40:49 -0500

    format header files

--- include/matrixOperations.h
+++ include/matrixOperations.h
@@ -3,14 +3,15 @@
 #include "matrix.h"
 #include <cstdint>
 namespace sclMath {
-  matrix copyMatrix(const matrix&);
-  ComplexScalar innerProduct(const matrix&, const matrix&);
-  Scalar distance (const matrix&, const matrix&);
-  Scalar angle (const matrix&, const matrix&);
+matrix copyMatrix(const matrix &);
+ComplexScalar innerProduct(const matrix &, const matrix &);
+Scalar distance(const matrix &, const matrix &);
+Scalar angle(const matrix &, const matrix &);
 
-}
-  sclMath::matrix operator+ (const sclMath::matrix&, const sclMath::matrix&);
-  sclMath::matrix operator- (const sclMath::matrix&, const sclMath::matrix&);
-  sclMath::matrix operator* (const sclMath::matrix&, const sclMath::matrix&);
-  sclMath::matrix operator* (const sclMath::ComplexScalar, const sclMath::matrix&);
-  bool operator== (const sclMath::matrix&, const sclMath::matrix&);
+} // namespace sclMath
+sclMath::matrix operator+(const sclMath::matrix &, const sclMath::matrix &);
+sclMath::matrix operator-(const sclMath::matrix &, const sclMath::matrix &);
+sclMath::matrix operator*(const sclMath::matrix &, const sclMath::matrix &);
+sclMath::matrix operator*(const sclMath::ComplexScalar,
+                          const sclMath::matrix &);
+bool operator==(const sclMath::matrix &, const sclMath::matrix &);
