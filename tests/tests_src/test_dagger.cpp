6dd14042eca080b99b3becfb4b5c62874144ec11
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-16 22:38:33 -0500

    update test to support new api, and realMatrix

--- tests/tests_src/test_dagger.cpp
+++ tests/tests_src/test_dagger.cpp
@@ -1,65 +1,109 @@
 
 
-
-#include <catch2/catch_test_macros.hpp>
 #include "sclMatrix.hpp"
 #include "sclMatrixOperations.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-#include <iostream>
-SCENARIO("dagger of matrix","[dagger]" )
-{
-  GIVEN("A comlex matrix of shape (3,4)"){
-    sclMath::matrix m (3,4);
-    //check the shape
-    REQUIRE(m.getRows()==3);
-    REQUIRE(m.getCols()==4);
+SCENARIO("dagger of matrix", "[dagger]") {
+  GIVEN("A comlex matrix of shape (3,4)") {
+    sclMath::ComplexMatrix m(3, 4);
+    // check the shape
+    REQUIRE(m.getRows() == 3);
+    REQUIRE(m.getCols() == 4);
 
-    //setting up the state of the matrix
-    m.set(0,0,sclMath::ComplexScalar(0,1)); 
-    m.set(1,3,sclMath::ComplexScalar(1,1)); 
-    m.set(2,1,sclMath::ComplexScalar(2,1.1)); 
-    m.set(2,0,sclMath::ComplexScalar(0.5,1.2)); 
+    // setting up the state of the matrix
+    m.set(0, 0, sclMath::ComplexScalar(0, 1));
+    m.set(1, 3, sclMath::ComplexScalar(1, 1));
+    m.set(2, 1, sclMath::ComplexScalar(2, 1.1));
+    m.set(2, 0, sclMath::ComplexScalar(0.5, 1.2));
 
-    m.set(2,2,sclMath::ComplexScalar(0,-1.1)); 
-    m.set(0,2,sclMath::ComplexScalar(-9,1)); 
-    m.set(1,2,sclMath::ComplexScalar(-0.4,1.3)); 
-    m.set(1,1,sclMath::ComplexScalar(0.2,1.2)); 
-    
-    WHEN ("when the matrix is transposed"){
+    m.set(2, 2, sclMath::ComplexScalar(0, -1.1));
+    m.set(0, 2, sclMath::ComplexScalar(-9, 1));
+    m.set(1, 2, sclMath::ComplexScalar(-0.4, 1.3));
+    m.set(1, 1, sclMath::ComplexScalar(0.2, 1.2));
+
+    WHEN("when the matrix is transposed") {
 
       m.dagger();
 
-      THEN("swaps m[i,j] with m[j,i]"){
-        REQUIRE(m.get(0,0)==sclMath::ComplexScalar(0,-1)); 
-        REQUIRE(m.get(3,1)==sclMath::ComplexScalar(1,-1)); 
-        REQUIRE(m.get(1,2)==sclMath::ComplexScalar(2,-1.1)); 
-        REQUIRE(m.get(0,2)==sclMath::ComplexScalar(0.5,-1.2)); 
-                       
-        REQUIRE(m.get(2,2)==sclMath::ComplexScalar(0,1.1)); 
-        REQUIRE(m.get(2,0)==sclMath::ComplexScalar(-9,-1)); 
-        REQUIRE(m.get(2,1)==sclMath::ComplexScalar(-0.4,-1.3)); 
-        REQUIRE(m.get(1,1)==sclMath::ComplexScalar(0.2,-1.2)); 
+      THEN("swaps m[i,j] with m[j,i]") {
+        REQUIRE(m.get(0, 0) == sclMath::ComplexScalar(0, -1));
+        REQUIRE(m.get(3, 1) == sclMath::ComplexScalar(1, -1));
+        REQUIRE(m.get(1, 2) == sclMath::ComplexScalar(2, -1.1));
+        REQUIRE(m.get(0, 2) == sclMath::ComplexScalar(0.5, -1.2));
 
+        REQUIRE(m.get(2, 2) == sclMath::ComplexScalar(0, 1.1));
+        REQUIRE(m.get(2, 0) == sclMath::ComplexScalar(-9, -1));
+        REQUIRE(m.get(2, 1) == sclMath::ComplexScalar(-0.4, -1.3));
+        REQUIRE(m.get(1, 1) == sclMath::ComplexScalar(0.2, -1.2));
       }
-
     }
-    WHEN ("the matrix is conjugated twice "){
+    WHEN("the matrix is conjugated twice ") {
       m.dagger().dagger();
 
-      THEN("the value at each position should e the same "){
-        REQUIRE(m.get(0,0)==sclMath::ComplexScalar(0,1)); 
-        REQUIRE(m.get(1,3)==sclMath::ComplexScalar(1,1)); 
-        REQUIRE(m.get(2,1)==sclMath::ComplexScalar(2,1.1)); 
-        REQUIRE(m.get(2,0)==sclMath::ComplexScalar(0.5,1.2)); 
+      THEN("the value at each position should e the same ") {
+        REQUIRE(m.get(0, 0) == sclMath::ComplexScalar(0, 1));
+        REQUIRE(m.get(1, 3) == sclMath::ComplexScalar(1, 1));
+        REQUIRE(m.get(2, 1) == sclMath::ComplexScalar(2, 1.1));
+        REQUIRE(m.get(2, 0) == sclMath::ComplexScalar(0.5, 1.2));
 
-        REQUIRE(m.get(2,2)==sclMath::ComplexScalar(0,-1.1)); 
-        REQUIRE(m.get(0,2)==sclMath::ComplexScalar(-9,1)); 
-        REQUIRE(m.get(1,2)==sclMath::ComplexScalar(-0.4,1.3)); 
-        REQUIRE(m.get(1,1)==sclMath::ComplexScalar(0.2,1.2)); 
+        REQUIRE(m.get(2, 2) == sclMath::ComplexScalar(0, -1.1));
+        REQUIRE(m.get(0, 2) == sclMath::ComplexScalar(-9, 1));
+        REQUIRE(m.get(1, 2) == sclMath::ComplexScalar(-0.4, 1.3));
+        REQUIRE(m.get(1, 1) == sclMath::ComplexScalar(0.2, 1.2));
+      }
+    }
+  }
+  GIVEN("A real matrix of shape (3,4)") {
+    sclMath::RealMatrix m(3, 4);
+    // check the shape
+    REQUIRE(m.getRows() == 3);
+    REQUIRE(m.getCols() == 4);
 
+    // setting up the state of the matrix
+    m.set(0, 0, 4);
+    m.set(1, 3, 1);
+    m.set(2, 1, 2);
+    m.set(2, 0, 7);
+    ;
+    m.set(2, 2, 0);
+    m.set(0, 2, -9);
+    m.set(1, 2, -1);
+    m.set(1, 1, 0.1);
+
+    WHEN("when the matrix is transposed") {
+
+      m.dagger();
+
+      THEN("swaps m[i,j] with m[j,i]") {
+        REQUIRE(m.get(0, 0) == 4);
+        REQUIRE(m.get(3, 1) == 1);
+        REQUIRE(m.get(1, 2) == 2);
+        REQUIRE(m.get(0, 2) == 7);
+
+        REQUIRE(m.get(2, 2) == 0);
+        REQUIRE(m.get(2, 0) == -9);
+        REQUIRE(m.get(2, 1) == -1);
+        REQUIRE(m.get(1, 1) == 0.1);
+      }
+    }
+    WHEN("the matrix is conjugated twice ") {
+      m.dagger().dagger();
+
+      THEN("the value at each position should e the same ") {
+        REQUIRE(m.get(0, 0) == 4);
+        REQUIRE(m.get(1, 3) == 1);
+        REQUIRE(m.get(2, 1) == 2);
+        REQUIRE(m.get(2, 0) == 7);
+
+        REQUIRE(m.get(2, 2) == 0);
+        REQUIRE(m.get(0, 2) == -9);
+        REQUIRE(m.get(1, 2) == -1);
+        REQUIRE(m.get(1, 1) == 0.1);
       }
     }
   }
 }
+// TODO:fix test messages
