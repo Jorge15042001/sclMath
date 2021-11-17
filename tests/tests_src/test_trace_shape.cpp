9eb7a1df0292443977c71e2185eb42e3f970a787
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-17 01:26:56 -0500

    adding tests for interaction between real and complex matrices

--- tests/tests_src/test_trace_shape.cpp
+++ tests/tests_src/test_trace_shape.cpp
@@ -1,37 +1,58 @@
-#include <catch2/catch_test_macros.hpp>
 #include "sclMatrix.hpp"
 #include "sclMatrixOperations.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-#include <iostream>
-TEST_CASE("trace shape of non-square matrix","[trace]" )
-{
-  sclMath::matrix m1 (1,2);
-  sclMath::matrix m2 (3,1);
-  sclMath::matrix m3 (7,8);
-  sclMath::matrix m4 (9,4);
-  sclMath::matrix m5 (2,9);
+TEST_CASE("trace shape of non-square complexmatrix", "[trace]") {
+  sclMath::ComplexMatrix m1(1, 2);
+  sclMath::ComplexMatrix m2(3, 1);
+  sclMath::ComplexMatrix m3(7, 8);
+  sclMath::ComplexMatrix m4(9, 4);
+  sclMath::ComplexMatrix m5(2, 9);
 
   CHECK_THROWS(m1.trace());
   CHECK_THROWS(m2.trace());
   CHECK_THROWS(m3.trace());
   CHECK_THROWS(m4.trace());
   CHECK_THROWS(m5.trace());
-
 }
-TEST_CASE("trace shape of suare matrix","[trace]" )
-{
-  sclMath::matrix m1 (1,1);
-  sclMath::matrix m2 (3,3);
-  sclMath::matrix m3 (7,7);
-  sclMath::matrix m4 (9,9);
-  sclMath::matrix m5 (2,2);
+TEST_CASE("trace shape of suare complexmatrix", "[trace]") {
+  sclMath::ComplexMatrix m1(1, 1);
+  sclMath::ComplexMatrix m2(3, 3);
+  sclMath::ComplexMatrix m3(7, 7);
+  sclMath::ComplexMatrix m4(9, 9);
+  sclMath::ComplexMatrix m5(2, 2);
 
   CHECK_NOTHROW(m1.trace());
   CHECK_NOTHROW(m2.trace());
   CHECK_NOTHROW(m3.trace());
   CHECK_NOTHROW(m4.trace());
   CHECK_NOTHROW(m5.trace());
+}
+TEST_CASE("trace shape of non-square RealMatrix", "[trace]") {
+  sclMath::RealMatrix m1(1, 2);
+  sclMath::RealMatrix m2(3, 1);
+  sclMath::RealMatrix m3(7, 8);
+  sclMath::RealMatrix m4(9, 4);
+  sclMath::RealMatrix m5(2, 9);
 
+  CHECK_THROWS(m1.trace());
+  CHECK_THROWS(m2.trace());
+  CHECK_THROWS(m3.trace());
+  CHECK_THROWS(m4.trace());
+  CHECK_THROWS(m5.trace());
+}
+TEST_CASE("trace shape of suare RealMatrix", "[trace]") {
+  sclMath::RealMatrix m1(1, 1);
+  sclMath::RealMatrix m2(3, 3);
+  sclMath::RealMatrix m3(7, 7);
+  sclMath::RealMatrix m4(9, 9);
+  sclMath::RealMatrix m5(2, 2);
+
+  CHECK_NOTHROW(m1.trace());
+  CHECK_NOTHROW(m2.trace());
+  CHECK_NOTHROW(m3.trace());
+  CHECK_NOTHROW(m4.trace());
+  CHECK_NOTHROW(m5.trace());
 }
