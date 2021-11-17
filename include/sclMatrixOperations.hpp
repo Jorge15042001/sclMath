eb11232a77665d8c53d4f3db39e964ebc83f5243
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-17 00:05:42 -0500

    overloading operator== between real and complex matrices

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -105,11 +105,15 @@ sclMath::ComplexMatrix operator*(const sclMath::ComplexScalar s,
 // ###########################################################################################################
 // TODO: almost equal neeeds to be tested with complex numbers
 // TODO: write a test for this function
-// TODO: overloading == between sclMath::ComplexMatrix , sclMath::RealMatrix
 template <sclMath::c_ScalarType T_SCALAR>
 bool operator==(const sclMath::matrix<T_SCALAR> &m1,
                 const sclMath::matrix<T_SCALAR> &m2);
-
+// overloading operator == for sclMath::RealMatrix and sclMath::ComplexMatrix
+bool operator==(const sclMath::RealMatrix &m1,
+                const sclMath::ComplexMatrix &m2);
+// overloading operator == for sclMath::ComplexMatrix and sclMath::RealMatrix
+bool operator==(const sclMath::ComplexMatrix &m1,
+                const sclMath::RealMatrix &m2);
 namespace sclMath {
 // implementation
 // ###########################################################################################################
@@ -409,7 +413,7 @@ sclMath::ComplexMatrix operator*(const sclMath::ComplexScalar s,
 }
 // TODO: almost equal neeeds to be tested with complex numbers
 // TODO: write a test for this function
-// TODO: overloading == between sclMath::ComplexMatrix , sclMath::RealMatrix
+// TODO:DONE overloading == between sclMath::ComplexMatrix , sclMath::RealMatrix
 template <sclMath::c_ScalarType T_SCALAR>
 bool operator==(const sclMath::matrix<T_SCALAR> &m1,
                 const sclMath::matrix<T_SCALAR> &m2) {
@@ -428,4 +432,28 @@ bool operator==(const sclMath::matrix<T_SCALAR> &m1,
     }
   }
   return true;
+}
+// overloading operator == for sclMath::RealMatrix and sclMath::ComplexMatrix
+bool operator==(const sclMath::RealMatrix &m1,
+                const sclMath::ComplexMatrix &m2) {
+  if (m1.getRows() != m2.getRows())
+    return false;
+  if (m1.getCols() != m2.getCols())
+    return false;
+
+  const std::size_t rows = m1.getRows();
+  const std::size_t cols = m1.getCols();
+
+  for (std::size_t i = 0; i < rows; i++) {
+    for (std::size_t j = 0; j < cols; j++) {
+      if (!sclMathUtility::areaAlmostEqual(m1.get(i, j), m2.get(i, j)))
+        return false;
+    }
+  }
+  return true;
+}
+// overloading operator == for sclMath::ComplexMatrix and sclMath::RealMatrix
+bool operator==(const sclMath::ComplexMatrix &m1,
+                const sclMath::RealMatrix &m2) {
+  return m2 == m1;
 }
--- tests/tests_src/test_matrixMultiplication.cpp
+++ tests/tests_src/test_matrixMultiplication.cpp
@@ -145,4 +145,75 @@ SCENARIO("matrix multiplication result", "[result]") {
       }
     }
   }
+  GIVEN("real matrices and complex matrices of compatible sizes") {
+    // TODO: write indentity matrix class;
+    sclMath::ComplexMatrix identity(4, 4);
+    // TODO: write zero matrix class
+    sclMath::RealMatrix zero_4_4(4, 4);
+    sclMath::ComplexMatrix zero_3_4(3, 4);
+
+    sclMath::RealMatrix m1(3, 4);
+    sclMath::ComplexMatrix m2(4, 4);
+
+    m1.set(0, 0, 1);
+    m1.set(0, 2, 0);
+    m1.set(0, 3, 1);
+    m1.set(1, 0, 3);
+    m1.set(2, 0, 1);
+    m1.set(2, 3, 3);
+
+    m2.set(0, 1, {2, 1});
+    m2.set(0, 2, {0, 1});
+    m2.set(0, 3, {3, -1});
+    m2.set(1, 1, {1, -2});
+    m2.set(2, 1, {2, 4});
+    m2.set(2, 2, {1, -4});
+
+    identity.set(0, 0, 1);
+    identity.set(1, 1, 1);
+    identity.set(2, 2, 1);
+    identity.set(3, 3, 1);
+
+    WHEN("a matrix is multiplied by the identity") {
+      const sclMath::ComplexMatrix copyOfm1 = m1 * identity;
+      const sclMath::ComplexMatrix copyOfm2 = m2 * identity;
+
+      THEN("is the same matrix") {
+        REQUIRE(m1 == copyOfm1);
+        REQUIRE(m2 == copyOfm2);
+      }
+    }
+    WHEN("is multiplied by 0") {
+      const sclMath::RealMatrix copyOfm1 = m1 * zero_4_4;
+      const sclMath::ComplexMatrix copyOfm2 = m2 * zero_4_4;
+
+      THEN("is the same as zero") {
+        REQUIRE(zero_3_4 == copyOfm1);
+        REQUIRE(zero_4_4 == copyOfm2);
+      }
+    }
+    WHEN("A and b are multiplied togeter") {
+      const sclMath::ComplexMatrix result = m1 * m2;
+
+      // TODO: complete the test
+      THEN("the result should match the definition of matrix multiplication") {
+        // TODO: easier api to set this values is required
+        sclMath::ComplexMatrix expectedResult(3, 4);
+
+        expectedResult.set(0, 1, {2, 1});
+        expectedResult.set(0, 2, {0, 1});
+        expectedResult.set(0, 3, {3, -1});
+
+        expectedResult.set(1, 1, {6, 3});
+        expectedResult.set(1, 1, {0, 3});
+        expectedResult.set(1, 3, {9, -3});
+
+        expectedResult.set(2, 1, {2, 1});
+        expectedResult.set(2, 2, {0, 1});
+        expectedResult.set(2, 3, {3, -1});
+
+        REQUIRE(expectedResult == result);
+      }
+    }
+  }
 }
