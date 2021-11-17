8762c6a4e2a1c72bde901345e61be60e0eb77c95
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-17 01:11:37 -0500

    extedns tests to cover interaction between real and complexx matrices

--- tests/tests_src/test_sum_shape.cpp
+++ tests/tests_src/test_sum_shape.cpp
@@ -6,7 +6,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-TEST_CASE("sum of diferent shape matrix ", "[sum]") {
+TEST_CASE("sum of diferent shape ComplexMatrix ", "[sum]") {
   sclMath::ComplexMatrix m1(1, 2);
   sclMath::ComplexMatrix m2(3, 1);
   sclMath::ComplexMatrix m3(7, 8);
@@ -19,7 +19,24 @@ TEST_CASE("sum of diferent shape matrix ", "[sum]") {
   CHECK_THROWS(m3 + m5);
   CHECK_THROWS(m2 + m4);
 }
-TEST_CASE("sum  of same shape matrix", "[sum]") {
+
+TEST_CASE("sum of compatible non-square shape ComplexMatrix ", "[sum]") {
+
+  sclMath::ComplexMatrix m1(3, 2);
+  sclMath::ComplexMatrix m2(3, 2);
+  sclMath::ComplexMatrix m3(4, 8);
+  sclMath::ComplexMatrix m4(4, 8);
+  sclMath::ComplexMatrix m5(2, 9);
+  sclMath::ComplexMatrix m6(2, 9);
+
+  CHECK_NOTHROW(m1 + m2);
+  CHECK_NOTHROW(m2 + m1);
+  CHECK_NOTHROW(m3 + m4);
+  CHECK_NOTHROW(m4 + m4);
+  CHECK_NOTHROW(m5 + m6);
+  CHECK_NOTHROW(m6 + m5);
+}
+TEST_CASE("sum  of same shape ComplexMatrix", "[sum]") {
   sclMath::ComplexMatrix m1(1, 1);
   sclMath::ComplexMatrix m2(3, 3);
   sclMath::ComplexMatrix m3(7, 7);
@@ -32,7 +49,7 @@ TEST_CASE("sum  of same shape matrix", "[sum]") {
   CHECK_NOTHROW(m4 + m4);
   CHECK_NOTHROW(m5 + m5);
 }
-TEST_CASE("sum right output shape", "[sum]") {
+TEST_CASE("sum right output ComplexMatrix", "[sum]") {
   sclMath::ComplexMatrix m1(1, 1);
   sclMath::ComplexMatrix m2(3, 3);
   sclMath::ComplexMatrix m3(7, 7);
@@ -61,7 +78,7 @@ TEST_CASE("sum right output shape", "[sum]") {
   CHECK(m5.getCols() == m5Result.getCols());
 }
 // tests for real matrices
-TEST_CASE("sum of diferent shape matrix ", "[sum]") {
+TEST_CASE("sum of diferent shape RealMatrix ", "[sum]") {
   sclMath::RealMatrix m1(1, 2);
   sclMath::RealMatrix m2(3, 1);
   sclMath::RealMatrix m3(7, 8);
@@ -74,7 +91,23 @@ TEST_CASE("sum of diferent shape matrix ", "[sum]") {
   CHECK_THROWS(m3 + m5);
   CHECK_THROWS(m2 + m4);
 }
-TEST_CASE("sum  of same shape matrix", "[sum]") {
+TEST_CASE("sum of compatible non-square shape RealMatrix ", "[sum]") {
+
+  sclMath::RealMatrix m1(3, 2);
+  sclMath::RealMatrix m2(3, 2);
+  sclMath::RealMatrix m3(4, 8);
+  sclMath::RealMatrix m4(4, 8);
+  sclMath::RealMatrix m5(2, 9);
+  sclMath::RealMatrix m6(2, 9);
+
+  CHECK_NOTHROW(m1 + m2);
+  CHECK_NOTHROW(m2 + m1);
+  CHECK_NOTHROW(m3 + m4);
+  CHECK_NOTHROW(m4 + m4);
+  CHECK_NOTHROW(m5 + m6);
+  CHECK_NOTHROW(m6 + m5);
+}
+TEST_CASE("sum  of same shape RealMatrix", "[sum]") {
   sclMath::RealMatrix m1(1, 1);
   sclMath::RealMatrix m2(3, 3);
   sclMath::RealMatrix m3(7, 7);
@@ -87,7 +120,7 @@ TEST_CASE("sum  of same shape matrix", "[sum]") {
   CHECK_NOTHROW(m4 + m4);
   CHECK_NOTHROW(m5 + m5);
 }
-TEST_CASE("sum right output shape", "[sum]") {
+TEST_CASE("sum right output RealMatrix", "[sum]") {
   sclMath::RealMatrix m1(1, 1);
   sclMath::RealMatrix m2(3, 3);
   sclMath::RealMatrix m3(7, 7);
@@ -99,6 +132,77 @@ TEST_CASE("sum right output shape", "[sum]") {
   sclMath::RealMatrix m3Result = m3 + m3;
   sclMath::RealMatrix m4Result = m4 + m4;
   sclMath::RealMatrix m5Result = m5 + m5;
+
+  CHECK(m1.getRows() == m1Result.getRows());
+  CHECK(m1.getCols() == m1Result.getCols());
+
+  CHECK(m2.getRows() == m2Result.getRows());
+  CHECK(m2.getCols() == m2Result.getCols());
+
+  CHECK(m3.getRows() == m3Result.getRows());
+  CHECK(m3.getCols() == m3Result.getCols());
+
+  CHECK(m4.getRows() == m4Result.getRows());
+  CHECK(m4.getCols() == m4Result.getCols());
+
+  CHECK(m5.getRows() == m5Result.getRows());
+  CHECK(m5.getCols() == m5Result.getCols());
+}
+TEST_CASE("sum of diferent shape RealMatrix with ComplexMatrix", "[sum]") {
+  sclMath::RealMatrix m1(1, 2);
+  sclMath::ComplexMatrix m2(3, 1);
+  sclMath::RealMatrix m3(7, 8);
+  sclMath::ComplexMatrix m4(9, 4);
+  sclMath::RealMatrix m5(2, 9);
+
+  CHECK_THROWS(m1 + m2);
+  CHECK_THROWS(m2 + m3);
+  CHECK_THROWS(m1 + m3);
+  CHECK_THROWS(m3 + m5);
+  CHECK_THROWS(m2 + m4);
+}
+TEST_CASE("sum of compatible non-square shape RealMatrix with ComplexMatrix",
+          "[sum]") {
+
+  sclMath::ComplexMatrix m1(3, 2);
+  sclMath::RealMatrix m2(3, 2);
+  sclMath::RealMatrix m3(4, 8);
+  sclMath::ComplexMatrix m4(4, 8);
+  sclMath::RealMatrix m5(2, 9);
+  sclMath::ComplexMatrix m6(2, 9);
+
+  CHECK_NOTHROW(m1 + m2);
+  CHECK_NOTHROW(m2 + m1);
+  CHECK_NOTHROW(m3 + m4);
+  CHECK_NOTHROW(m4 + m4);
+  CHECK_NOTHROW(m5 + m6);
+  CHECK_NOTHROW(m6 + m5);
+}
+TEST_CASE("sum  of same shape RealMatrix with ComplexMatrix", "[sum]") {
+  sclMath::ComplexMatrix m1(1, 1);
+  sclMath::RealMatrix m2(3, 3);
+  sclMath::ComplexMatrix m3(7, 7);
+  sclMath::ComplexMatrix m4(9, 9);
+  sclMath::RealMatrix m5(2, 2);
+
+  CHECK_NOTHROW(m1 + m1);
+  CHECK_NOTHROW(m2 + m2);
+  CHECK_NOTHROW(m3 + m3);
+  CHECK_NOTHROW(m4 + m4);
+  CHECK_NOTHROW(m5 + m5);
+}
+TEST_CASE("sum right output RealMatrix with ComplexMatrix", "[sum]") {
+  sclMath::ComplexMatrix m1(1, 1);
+  sclMath::RealMatrix m2(3, 3);
+  sclMath::ComplexMatrix m3(7, 7);
+  sclMath::RealMatrix m4(9, 9);
+  sclMath::ComplexMatrix m5(2, 2);
+
+  sclMath::matrix m1Result = m1 + m1;
+  sclMath::matrix m2Result = m2 + m2;
+  sclMath::matrix m3Result = m3 + m3;
+  sclMath::matrix m4Result = m4 + m4;
+  sclMath::matrix m5Result = m5 + m5;
 
   CHECK(m1.getRows() == m1Result.getRows());
   CHECK(m1.getCols() == m1Result.getCols());
