02cdbf3ac90d84f74e497b2765b26c8632bc36ed
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-16 22:45:45 -0500

    update test to support new api, and realMatrix

--- tests/tests_src/test_get.cpp
+++ tests/tests_src/test_get.cpp
@@ -1,93 +1,172 @@
-#include <catch2/catch_test_macros.hpp>
 #include "sclMatrix.hpp"
 #include "sclMatrixOperations.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <vector>
 
-SCENARIO("getting from matrix","[get]" )
-{
-  GIVEN("A comlex matrix of shape (4,4)"){
-    sclMath::matrix m (4,4);
-    //check the shape
-    REQUIRE(m.getRows()==4);
-    REQUIRE(m.getCols()==4);
-    
-    WHEN("it has been created"){
-      ;//matrix was initialized in the contstuctor
-      THEN("all values must be initialized as 0+0j"){
-        REQUIRE(m.get(0,0)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(0,1)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(0,2)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(0,3)==sclMath::ComplexScalar(0,0));
+SCENARIO("getting from matrix", "[get]") {
+  GIVEN("A comlex matrix of shape (4,4)") {
+    sclMath::ComplexMatrix m(4, 4);
+    // check the shape
+    REQUIRE(m.getRows() == 4);
+    REQUIRE(m.getCols() == 4);
 
-        REQUIRE(m.get(1,0)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(1,1)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(1,2)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(1,3)==sclMath::ComplexScalar(0,0));
+    WHEN("it has been created") {
+      ; // matrix was initialized in the contstuctor
+      THEN("all values must be initialized as 0+0j") {
+        REQUIRE(m.get(0, 0) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(0, 1) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(0, 2) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(0, 3) == sclMath::ComplexScalar(0, 0));
 
-        REQUIRE(m.get(2,0)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(2,1)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(2,2)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(2,3)==sclMath::ComplexScalar(0,0));
+        REQUIRE(m.get(1, 0) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(1, 1) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(1, 2) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(1, 3) == sclMath::ComplexScalar(0, 0));
 
-        REQUIRE(m.get(3,0)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(3,1)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(3,2)==sclMath::ComplexScalar(0,0));
-        REQUIRE(m.get(3,3)==sclMath::ComplexScalar(0,0));
+        REQUIRE(m.get(2, 0) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(2, 1) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(2, 2) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(2, 3) == sclMath::ComplexScalar(0, 0));
+
+        REQUIRE(m.get(3, 0) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(3, 1) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(3, 2) == sclMath::ComplexScalar(0, 0));
+        REQUIRE(m.get(3, 3) == sclMath::ComplexScalar(0, 0));
       }
-      
     }
-    WHEN("index out of range range is requested"){
+    WHEN("index out of range range is requested") {
       ;
-      THEN("error should be thrown"){
-        CHECK_THROWS(m.get(1,5));
-        CHECK_THROWS(m.get(5,4));
-        CHECK_THROWS(m.get(5,5));
+      THEN("error should be thrown") {
+        CHECK_THROWS(m.get(1, 5));
+        CHECK_THROWS(m.get(5, 4));
+        CHECK_THROWS(m.get(5, 5));
 
-        CHECK_THROWS(m.get(1,6));
-        CHECK_THROWS(m.get(8,7));
-        CHECK_THROWS(m.get(9,11));
+        CHECK_THROWS(m.get(1, 6));
+        CHECK_THROWS(m.get(8, 7));
+        CHECK_THROWS(m.get(9, 11));
       }
     }
-    WHEN("a negative index is requested"){
+    WHEN("a negative index is requested") {
       ;
-      THEN("error should bw thrown"){
-        CHECK_THROWS(m.get(-1,5));
-        CHECK_THROWS(m.get(-1,-5));
-        CHECK_THROWS(m.get(3,-5));
-        CHECK_THROWS(m.get(-4,5));
-        CHECK_THROWS(m.get(-7,5));
-        CHECK_THROWS(m.get(7,-3));
-        CHECK_THROWS(m.get(6,-4));
-        CHECK_THROWS(m.get(-6,-4));
-        CHECK_THROWS(m.get(-6,-3));
+      THEN("error should bw thrown") {
+        CHECK_THROWS(m.get(-1, 5));
+        CHECK_THROWS(m.get(-1, -5));
+        CHECK_THROWS(m.get(3, -5));
+        CHECK_THROWS(m.get(-4, 5));
+        CHECK_THROWS(m.get(-7, 5));
+        CHECK_THROWS(m.get(7, -3));
+        CHECK_THROWS(m.get(6, -4));
+        CHECK_THROWS(m.get(-6, -4));
+        CHECK_THROWS(m.get(-6, -3));
       }
     }
-    WHEN("a valid index in requested"){
+    WHEN("a valid index in requested") {
       ;
-      THEN("no errors should be throw "){
-        CHECK_NOTHROW(m.get(0,0));
-        CHECK_NOTHROW(m.get(0,1));
-        CHECK_NOTHROW(m.get(0,2));
-        CHECK_NOTHROW(m.get(0,3));
+      THEN("no errors should be throw ") {
+        CHECK_NOTHROW(m.get(0, 0));
+        CHECK_NOTHROW(m.get(0, 1));
+        CHECK_NOTHROW(m.get(0, 2));
+        CHECK_NOTHROW(m.get(0, 3));
 
-        CHECK_NOTHROW(m.get(1,0));
-        CHECK_NOTHROW(m.get(1,1));
-        CHECK_NOTHROW(m.get(1,2));
-        CHECK_NOTHROW(m.get(1,3));
+        CHECK_NOTHROW(m.get(1, 0));
+        CHECK_NOTHROW(m.get(1, 1));
+        CHECK_NOTHROW(m.get(1, 2));
+        CHECK_NOTHROW(m.get(1, 3));
 
-        CHECK_NOTHROW(m.get(2,0));
-        CHECK_NOTHROW(m.get(2,1));
-        CHECK_NOTHROW(m.get(2,2));
-        CHECK_NOTHROW(m.get(2,3));
+        CHECK_NOTHROW(m.get(2, 0));
+        CHECK_NOTHROW(m.get(2, 1));
+        CHECK_NOTHROW(m.get(2, 2));
+        CHECK_NOTHROW(m.get(2, 3));
 
-        CHECK_NOTHROW(m.get(3,0));
-        CHECK_NOTHROW(m.get(3,1));
-        CHECK_NOTHROW(m.get(3,2));
-        CHECK_NOTHROW(m.get(3,3));
-
+        CHECK_NOTHROW(m.get(3, 0));
+        CHECK_NOTHROW(m.get(3, 1));
+        CHECK_NOTHROW(m.get(3, 2));
+        CHECK_NOTHROW(m.get(3, 3));
       }
     }
+  }
+  GIVEN("A realMatrix of shape (4,4)") {
+    sclMath::RealMatrix m(4, 4);
+    // check the shape
+    REQUIRE(m.getRows() == 4);
+    REQUIRE(m.getCols() == 4);
 
+    WHEN("it has been created") {
+      ; // matrix was initialized in the contstuctor
+      THEN("all values should be initialized to 0") {
+        REQUIRE(m.get(0, 1) == 0);
+        REQUIRE(m.get(0, 0) == 0);
+        REQUIRE(m.get(0, 2) == 0);
+        REQUIRE(m.get(0, 3) == 0);
+
+        REQUIRE(m.get(1, 0) == 0);
+        REQUIRE(m.get(1, 1) == 0);
+        REQUIRE(m.get(1, 2) == 0);
+        REQUIRE(m.get(1, 3) == 0);
+
+        REQUIRE(m.get(2, 0) == 0);
+        REQUIRE(m.get(2, 1) == 0);
+        REQUIRE(m.get(2, 2) == 0);
+        REQUIRE(m.get(2, 3) == 0);
+
+        REQUIRE(m.get(3, 0) == 0);
+        REQUIRE(m.get(3, 1) == 0);
+        REQUIRE(m.get(3, 2) == 0);
+        REQUIRE(m.get(3, 3) == 0);
+      }
+    }
+    WHEN("index out of range range is requested") {
+      ;
+      THEN("error should be thrown") {
+        CHECK_THROWS(m.get(1, 5));
+        CHECK_THROWS(m.get(5, 4));
+        CHECK_THROWS(m.get(5, 5));
+
+        THEN("all values must be initialized as 0+0j") {
+          CHECK_THROWS(m.get(1, 6));
+          CHECK_THROWS(m.get(8, 7));
+          CHECK_THROWS(m.get(9, 11));
+        }
+      }
+      WHEN("a negative index is requested") {
+        ;
+        THEN("error should bw thrown") {
+          CHECK_THROWS(m.get(-1, 5));
+          CHECK_THROWS(m.get(-1, -5));
+          CHECK_THROWS(m.get(3, -5));
+          CHECK_THROWS(m.get(-4, 5));
+          CHECK_THROWS(m.get(-7, 5));
+          CHECK_THROWS(m.get(7, -3));
+          CHECK_THROWS(m.get(6, -4));
+          CHECK_THROWS(m.get(-6, -4));
+          CHECK_THROWS(m.get(-6, -3));
+        }
+      }
+      WHEN("a valid index in requested") {
+        ;
+        THEN("no errors should be throw ") {
+          CHECK_NOTHROW(m.get(0, 0));
+          CHECK_NOTHROW(m.get(0, 1));
+          CHECK_NOTHROW(m.get(0, 2));
+          CHECK_NOTHROW(m.get(0, 3));
+
+          CHECK_NOTHROW(m.get(1, 0));
+          CHECK_NOTHROW(m.get(1, 1));
+          CHECK_NOTHROW(m.get(1, 2));
+          CHECK_NOTHROW(m.get(1, 3));
+
+          CHECK_NOTHROW(m.get(2, 0));
+          CHECK_NOTHROW(m.get(2, 1));
+          CHECK_NOTHROW(m.get(2, 2));
+          CHECK_NOTHROW(m.get(2, 3));
+
+          CHECK_NOTHROW(m.get(3, 0));
+          CHECK_NOTHROW(m.get(3, 1));
+          CHECK_NOTHROW(m.get(3, 2));
+          CHECK_NOTHROW(m.get(3, 3));
+        }
+      }
+    }
   }
 }
