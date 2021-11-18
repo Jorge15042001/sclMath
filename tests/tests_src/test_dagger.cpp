69365378416772cbd50da89fba6d4fc24a6cb983
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-18 10:24:26 -0500

    RealMatrix has no dagger operation

--- tests/tests_src/test_dagger.cpp
+++ tests/tests_src/test_dagger.cpp
@@ -56,54 +56,5 @@ SCENARIO("dagger of matrix", "[dagger]") {
       }
     }
   }
-  GIVEN("A real matrix of shape (3,4)") {
-    sclMath::RealMatrix m(3, 4);
-    // check the shape
-    REQUIRE(m.getRows() == 3);
-    REQUIRE(m.getCols() == 4);
-
-    // setting up the state of the matrix
-    m.set(0, 0, 4);
-    m.set(1, 3, 1);
-    m.set(2, 1, 2);
-    m.set(2, 0, 7);
-    ;
-    m.set(2, 2, 0);
-    m.set(0, 2, -9);
-    m.set(1, 2, -1);
-    m.set(1, 1, 0.1);
-
-    WHEN("the dagger operation is perform on a RealMatrix") {
-
-      m.dagger();
-
-      THEN("it acts as a transpose operation") {
-        REQUIRE(m.get(0, 0) == 4);
-        REQUIRE(m.get(3, 1) == 1);
-        REQUIRE(m.get(1, 2) == 2);
-        REQUIRE(m.get(0, 2) == 7);
-
-        REQUIRE(m.get(2, 2) == 0);
-        REQUIRE(m.get(2, 0) == -9);
-        REQUIRE(m.get(2, 1) == -1);
-        REQUIRE(m.get(1, 1) == 0.1);
-      }
-    }
-    WHEN("the dagger operation is perform twice on a RealMatrix") {
-      m.dagger().dagger();
-
-      THEN("the value at each position should e the same ") {
-        REQUIRE(m.get(0, 0) == 4);
-        REQUIRE(m.get(1, 3) == 1);
-        REQUIRE(m.get(2, 1) == 2);
-        REQUIRE(m.get(2, 0) == 7);
-
-        REQUIRE(m.get(2, 2) == 0);
-        REQUIRE(m.get(0, 2) == -9);
-        REQUIRE(m.get(1, 2) == -1);
-        REQUIRE(m.get(1, 1) == 0.1);
-      }
-    }
-  }
 }
 // TODO:fix test messages
