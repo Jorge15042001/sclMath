8bae4c673f06f823a2b66007069df2a55565ff43
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-18 03:52:15 -0500

    moving to non template implementation of realMatrix

--- src/sclMatrix.cpp
+++ src/sclMatrix.cpp
@@ -9,8 +9,7 @@
 // implementation
 
 namespace sclMath {
-template <c_ScalarType T_SCALAR>
-matrix<T_SCALAR>::matrix(const std::int64_t rows, const std::int64_t cols)
+RealMatrix::RealMatrix(const std::int64_t rows, const std::int64_t cols)
     : rows(rows), cols(cols), m_data(rows * cols) {
   sclMathError::ASSERT2(rows > 0,
                         " number of rows can not be negative or zero");
@@ -18,16 +17,109 @@ matrix<T_SCALAR>::matrix(const std::int64_t rows, const std::int64_t cols)
                         " number of columns can not be negative or zero");
 }
 
-template <c_ScalarType T_SCALAR> std::size_t matrix<T_SCALAR>::getRows() const {
-  return this->rows;
+std::size_t RealMatrix::getRows() const { return this->rows; }
+
+std::size_t RealMatrix::getCols() const { return this->cols; }
+
+RealMatrix &RealMatrix::transpose() {
+  // AT[i,] = A[j,i]
+  std::vector<RealScalar> new_m_data(this->m_data.size());
+
+  auto f_finalPosition = [this](const std::size_t x) {
+    return (x % this->cols) * this->rows + x / this->cols;
+  };
+
+  for (std::size_t i = 0; i < this->m_data.size(); i++) {
+    new_m_data[f_finalPosition(i)] = this->m_data[i];
+  }
+  this->m_data = new_m_data;
+
+  std::swap(this->cols, this->rows);
+  return *this;
 }
 
-template <c_ScalarType T_SCALAR> std::size_t matrix<T_SCALAR>::getCols() const {
-  return this->cols;
+RealScalar RealMatrix::trace() const {
+  sclMathError::ASSERT2(this->rows == this->cols,
+                        "trace() is not valid for non squared matrices");
+  RealScalar result = 0;
+  for (std::size_t i = 0; i < this->rows; i++) {
+    result += this->get(i, i);
+  }
+  return result;
 }
 
-template <c_ScalarType T_SCALAR>
-matrix<T_SCALAR> &matrix<T_SCALAR>::conjugate() {
+// TODO: test this function
+RealScalar RealMatrix::normSquared() const {
+  // TODO:replace with stl algorith?
+  RealScalar reuslt = 0;
+  for (const RealScalar s : this->m_data)
+    // TODO: check if std::conj(float) may cause perfonmance issues
+    reuslt += s * s;
+  return reuslt;
+}
+RealScalar RealMatrix::norm() const { return std::sqrt(this->normSquared()); }
+
+// TODO: no everload for sclMath::Scalar and sclMath::ComplexScalar
+RealMatrix &RealMatrix::scale(const RealScalar c) {
+  // TODO: range for loop, read as copy not as reference
+  for (RealScalar &s : this->m_data)
+    s *= c;
+  return *this;
+}
+
+RealScalar RealMatrix::get(const std::int64_t i, const std::int64_t j) const {
+  sclMathError::ASSERT2(i >= 0 && i < this->rows,
+                        "requested row index is out of range");
+  sclMathError::ASSERT2(j >= 0 && j < this->cols,
+                        "requested column index is out of range");
+
+  const std::size_t index = this->cols * i + j;
+
+  return this->m_data[index];
+}
+void RealMatrix::set(const std::size_t i, const std::size_t j,
+                     const RealScalar s) {
+  sclMathError::ASSERT2(i >= 0 && i < this->rows,
+                        "requested row index is out of range");
+  sclMathError::ASSERT2(j >= 0 && j < this->cols,
+                        "requested column index is out of range");
+
+  this->m_data[this->cols * i + j] = s;
+}
+
+std::ostream &operator<<(std::ostream &os, const RealMatrix &m) {
+  const std::size_t rows = m.getRows();
+  const std::size_t cols = m.getCols();
+
+  os << "sclMath::RealMatrix[" << rows << "," << cols << "]\n{";
+
+  for (std::size_t i = 0; i < rows; i++) {
+    os << "{";
+    for (std::size_t j = 0; j < cols; j++) {
+      os << m.get(i, j) << ",";
+    }
+    os << "}\n";
+  }
+
+  os << "}\n";
+  return os;
+}
+
+} // namespace sclMath
+namespace sclMath {
+RealMatrix::RealMatrix(const std::int64_t rows, const std::int64_t cols)
+    : rows(rows), cols(cols), m_data(rows * cols) {
+  sclMathError::ASSERT2(rows > 0,
+                        " number of rows can not be negative or zero");
+  sclMathError::ASSERT2(cols > 0,
+                        " number of columns can not be negative or zero");
+}
+
+std::size_t RealMatrix::getRows() const { return this->rows; }
+
+std::size_t RealMatrix::getCols() const { return this->cols; }
+
+RealMatrix &RealMatrix::conjugate() {
   for (auto &c : this->m_data) {
     c = std::conj(c);
   }
@@ -35,7 +127,7 @@ matrix<T_SCALAR> &matrix<T_SCALAR>::conjugate() {
 }
 
 template <c_ScalarType T_SCALAR>
-matrix<T_SCALAR> &matrix<T_SCALAR>::transpose() {
+RealMatrix<T_SCALAR> &RealMatrix<T_SCALAR>::transpose() {
   // AT[i,] = A[j,i]
   std::vector<T_SCALAR> new_m_data(this->m_data.size());
 
@@ -52,14 +144,15 @@ matrix<T_SCALAR> &matrix<T_SCALAR>::transpose() {
   return *this;
 }
 
-template <c_ScalarType T_SCALAR> matrix<T_SCALAR> &matrix<T_SCALAR>::dagger() {
+template <c_ScalarType T_SCALAR>
+RealMatrix<T_SCALAR> &RealMatrix<T_SCALAR>::dagger() {
   this->conjugate();
   this->transpose();
 
   return *this;
 }
 
-template <c_ScalarType T_SCALAR> T_SCALAR matrix<T_SCALAR>::trace() const {
+template <c_ScalarType T_SCALAR> T_SCALAR RealMatrix<T_SCALAR>::trace() const {
   sclMathError::ASSERT2(this->rows == this->cols,
                         "trace() is not valid for non squared matrices");
   T_SCALAR result = 0;
@@ -70,18 +163,19 @@ template <c_ScalarType T_SCALAR> T_SCALAR matrix<T_SCALAR>::trace() const {
 }
 
 template <c_ScalarType T_SCALAR>
-RealScalar matrix<T_SCALAR>::normSquared() const {
+RealScalar RealMatrix<T_SCALAR>::normSquared() const {
   RealScalar reuslt = 0;
   for (const ComplexScalar s : this->m_data)
     // TODO: check if std::conj(float) may cause perfonmance issues
     reuslt += (s * std::conj(s)).real();
   return reuslt;
 }
-template <c_ScalarType T_SCALAR> RealScalar matrix<T_SCALAR>::norm() const {
+template <c_ScalarType T_SCALAR> RealScalar RealMatrix<T_SCALAR>::norm() const {
   return std::sqrt(this->normSquared());
 }
 
-template <c_ScalarType T_SCALAR> bool matrix<T_SCALAR>::isHermitian() const {
+template <c_ScalarType T_SCALAR>
+bool RealMatrix<T_SCALAR>::isHermitian() const {
   // if is not square matrix return false;
   if (this->rows != this->cols)
     return false;
@@ -98,15 +192,15 @@ template <c_ScalarType T_SCALAR> bool matrix<T_SCALAR>::isHermitian() const {
 }
 // TODO: no everload for sclMath::Scalar and sclMath::ComplexScalar
 template <c_ScalarType T_SCALAR>
-matrix<T_SCALAR> &matrix<T_SCALAR>::scale(const T_SCALAR c) {
+RealMatrix<T_SCALAR> &RealMatrix<T_SCALAR>::scale(const T_SCALAR c) {
   for (T_SCALAR &s : this->m_data)
     s *= c;
   return *this;
 }
 
 template <c_ScalarType T_SCALAR>
-T_SCALAR matrix<T_SCALAR>::get(const std::int64_t i,
-                               const std::int64_t j) const {
+T_SCALAR RealMatrix<T_SCALAR>::get(const std::int64_t i,
+                                   const std::int64_t j) const {
   sclMathError::ASSERT2(i >= 0 && i < this->rows,
                         "requested row index is out of range");
   sclMathError::ASSERT2(j >= 0 && j < this->cols,
@@ -117,8 +211,8 @@ T_SCALAR matrix<T_SCALAR>::get(const std::int64_t i,
   return this->m_data[index];
 }
 template <c_ScalarType T_SCALAR>
-void matrix<T_SCALAR>::set(const std::size_t i, const std::size_t j,
-                           const T_SCALAR s) {
+void RealMatrix<T_SCALAR>::set(const std::size_t i, const std::size_t j,
+                               const T_SCALAR s) {
   sclMathError::ASSERT2(i >= 0 && i < this->rows,
                         "requested row index is out of range");
   sclMathError::ASSERT2(j >= 0 && j < this->cols,
@@ -128,7 +222,7 @@ void matrix<T_SCALAR>::set(const std::size_t i, const std::size_t j,
 }
 
 template <c_ScalarType T_SCALAR>
-std::ostream &operator<<(std::ostream &os, const matrix<T_SCALAR> &m) {
+std::ostream &operator<<(std::ostream &os, const RealMatrix<T_SCALAR> &m) {
   const std::size_t rows = m.getRows();
   const std::size_t cols = m.getCols();
 
