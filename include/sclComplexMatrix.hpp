ab5d878848cc429873ac3101d47146a6c66ca9bc
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:04:51 -0500

    split real and complex matrices in two different files

--- include/sclComplexMatrix.hpp
+++ include/sclComplexMatrix.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "sclMathErrors.hpp"
+#include "sclMathErrors.hpp" //TODO: remove unsed header
 #include <complex>
-#include <iostream>
-#include <memory>
-#include <type_traits>
+#include <iostream>    //TODO: remove
+#include <type_traits> //TODO:remove
 #include <vector>
 
 namespace sclMath {
@@ -42,31 +41,4 @@ public:
 };
 std::ostream &operator<<(std::ostream &os, const ComplexMatrix &m);
 
-class RealMatrix {
-private:
-  std::vector<RealScalar> m_data;
-  std::size_t rows;
-  std::size_t cols;
-
-public:
-  RealMatrix(const std::int64_t rows, const std::int64_t cols);
-
-  RealMatrix &transpose();
-
-  RealScalar trace() const;
-  RealScalar normSquared() const;
-  RealScalar norm() const;
-
-  // TODO: is transpose methood missing
-
-  RealMatrix &scale(const RealScalar s);
-
-  std::size_t getRows() const;
-  std::size_t getCols() const;
-
-  void set(const std::size_t i, const std::size_t j, const RealScalar s);
-
-  RealScalar get(const std::int64_t i, const std::int64_t j) const;
-};
-std::ostream &operator<<(std::ostream &os, const RealMatrix &m);
 } // namespace sclMath
