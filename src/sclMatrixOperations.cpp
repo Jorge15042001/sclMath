24ecd01361bac72c1cfa22df48ee809ab37ab273
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 02:12:03 -0500

    moving implementation of angle to its own file

--- src/sclMatrixOperations.cpp
+++ src/sclMatrixOperations.cpp
@@ -1,26 +1,4 @@
 #include "sclMatrixOperations.hpp"
-namespace sclMath {
-// implementation
-
-//  angle for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-//  angle for sclMath::RealMatrix, sclMath::RealMatrix
-RealScalar angle(const RealMatrix &m1, const RealMatrix &m2) {
-  const RealScalar m1Norm = m1.norm();
-  const RealScalar m2Norm = m2.norm();
-
-  const RealScalar innerP = innerProduct(m1, m2);
-
-  return std::acos(innerP / (m1Norm * m2Norm));
-}
-RealScalar angle(const ComplexMatrix &m1, const ComplexMatrix &m2) {
-  const RealScalar m1Norm = m1.norm();
-  const RealScalar m2Norm = m2.norm();
-
-  const ComplexScalar innerP = innerProduct(m1, m2);
-
-  return std::acos(innerP.real() / (m1Norm * m2Norm));
-}
-} // namespace sclMath
 
 // matrix addition
 // ###########################################################################################################
