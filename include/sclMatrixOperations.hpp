16504ce8341e1f70f6ccb2f59751dbc566fed622
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:44:44 -0500

    separation of addition from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "sclAngle.hpp"        //TODO: remove
 #include "sclCopyMatrix.hpp"   //TODO: remove
 #include "sclDistance.hpp"     //TODO remove
 #include "sclInnerProduct.hpp" //TODO: remove
@@ -9,21 +10,6 @@
 #include <cmath>
 #include <cstdint>
 #include <iostream>
-
-// matrix addition
-// ###########################################################################################################
-// overloading operator + for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator+(const sclMath::ComplexMatrix &m1,
-                                 const sclMath::ComplexMatrix &m2);
-// overloading operator + for sclMath::RealMatrix, sclMath::RealMatrix
-sclMath::RealMatrix operator+(const sclMath::RealMatrix &m1,
-                              const sclMath::RealMatrix &m2);
-// overloading operator + for sclMath::RealMatrix, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator+(const sclMath::RealMatrix &,
-                                 const sclMath::ComplexMatrix &);
-// overloading operator + for sclMath::ComplexMatrix, sclMath::RealMatrix
-sclMath::ComplexMatrix operator+(const sclMath::ComplexMatrix &m1,
-                                 const sclMath::RealMatrix &m2);
 
 // matrix Subtraction
 // ###########################################################################################################
