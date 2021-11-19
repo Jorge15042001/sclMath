99deb3d9377e46dc8e2a87a519dd6e9a54b3b45c
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:53:54 -0500

    separation of multiplication by scalar from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -13,21 +13,6 @@
 #include <cstdint>
 #include <iostream>
 
-// scale matric
-// ###########################################################################################################
-// overloading operator * for sclMath::ComplexScalar, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator*(const sclMath::ComplexScalar s,
-                                 const sclMath::ComplexMatrix &m);
-// overloading operator * for sclMath::Scalar, sclMath::RealMatrix
-sclMath::RealMatrix operator*(const sclMath::RealScalar s,
-                              const sclMath::RealMatrix &m);
-// overloading operator * for sclMath::Scalar, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator*(const sclMath::RealScalar s,
-                                 const sclMath::ComplexMatrix &m);
-// overloading operator * for sclMath::ComplexScalar, sclMath::RealMatrix
-sclMath::ComplexMatrix operator*(const sclMath::ComplexScalar s,
-                                 const sclMath::RealMatrix &m);
-
 // == operator for matrices
 // ###########################################################################################################
 // TODO: almost equal neeeds to be tested with complex numbers
