74721e882d94ccf062ec1a487574a98d5cfd0256
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 02:00:41 -0500

    importing independent header files

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
-#include "sclAngle.hpp"        //TODO: remove
+#include "sclAngle.hpp" //TODO: remove
+#include "sclComplexMatrix.hpp"
 #include "sclCopyMatrix.hpp"   //TODO: remove
 #include "sclDistance.hpp"     //TODO remove
 #include "sclInnerProduct.hpp" //TODO: remove
 #include "sclMathErrors.hpp"
 #include "sclMathUtility.hpp"
-#include "sclMatrix.hpp"
 #include "sclMatrixAddition.hpp"       //TODO: remove
 #include "sclMatrixComparison.hpp"     //TODO: remove
 #include "sclMatrixMultiplication.hpp" //TODO: remove
 #include "sclMatrixScaling.hpp"        //TODO: remove
 #include "sclMatrixSubtraction.hpp"    //TODO: remove
+#include "sclRealMatrix.hpp"
