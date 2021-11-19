de19559444e50d55e44316c761dd282db582045b
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:41:38 -0500

    separation of angle from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -10,17 +10,6 @@
 #include <cstdint>
 #include <iostream>
 
-namespace sclMath {
-
-// angle
-// ###########################################################################################################
-//  angle for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-RealScalar angle(const ComplexMatrix &m1, const ComplexMatrix &m2);
-//  angle for sclMath::RealMatrix, sclMath::RealMatrix
-RealScalar angle(const RealMatrix &m1, const RealMatrix &m2);
-
-} // namespace sclMath
-
 // matrix addition
 // ###########################################################################################################
 // overloading operator + for sclMath::ComplexMatrix, sclMath::ComplexMatrix
