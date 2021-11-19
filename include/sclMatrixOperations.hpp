acbdc9eabde6f4f4cf63ebc3ec46733eb8e856b2
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:39:02 -0500

    separation of distance from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -11,17 +11,6 @@
 
 namespace sclMath {
 
-// distance
-// ###########################################################################################################
-// distance for sclmath::complexmatrix, sclmath::complexmatrix
-RealScalar distance(const ComplexMatrix &m1, const ComplexMatrix &m2);
-// distance for sclmath::realmatrix, sclmath::realmatrix
-RealScalar distance(const RealMatrix &m1, const RealMatrix &m2);
-// distance for sclMath::ComplexMatrix, sclMath::RealMatrixu
-RealScalar distance(const ComplexMatrix &m1, const RealMatrix &m2);
-// distance for sclMath::RealMatrix, sclMath::ComplexMatrix
-RealScalar distance(const RealMatrix &m1, const ComplexMatrix &m2);
-
 // angle
 // ###########################################################################################################
 //  angle for sclMath::ComplexMatrix, sclMath::ComplexMatrix
