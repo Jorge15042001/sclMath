cf1467b774db21bbbbe2fecbfbd714e781b44493
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:51:51 -0500

    separation of multiplication from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -13,21 +13,6 @@
 #include <cstdint>
 #include <iostream>
 
-// matrix multiplication
-// ###########################################################################################################
-// overloading operator * for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator*(const sclMath::ComplexMatrix &m1,
-                                 const sclMath::ComplexMatrix &m2);
-// overloading operator * for sclMath::RealMatrix, sclMath::RealMatrix
-sclMath::RealMatrix operator*(const sclMath::RealMatrix &m1,
-                              const sclMath::RealMatrix &m2);
-// overloading operator * for sclMath::RealMatrix, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator*(const sclMath::RealMatrix &m1,
-                                 const sclMath::ComplexMatrix &m2);
-// overloading operator * for sclMath::ComplexMatrix, sclMath::RealMatrix
-sclMath::ComplexMatrix operator*(const sclMath::ComplexMatrix &m1,
-                                 const sclMath::RealMatrix &m2);
-
 // scale matric
 // ###########################################################################################################
 // overloading operator * for sclMath::ComplexScalar, sclMath::ComplexMatrix
