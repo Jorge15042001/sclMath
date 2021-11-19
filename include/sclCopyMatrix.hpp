bff7b250db68859d6eac718fa94b2847ad76e265
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 02:22:01 -0500

    fix: wrong function signature

--- include/sclCopyMatrix.hpp
+++ include/sclCopyMatrix.hpp
@@ -7,5 +7,5 @@ RealMatrix copyMatrix(const RealMatrix &m);
 // copy matrix for sclMath::ComplexMatrix, sclMath::ComplexMatrix
 ComplexMatrix copyMatrix(const ComplexMatrix &m);
 // TODO: copy real matrix as complex matrix, and viseversa
-RealMatrix copyMatrix_asComplex(const ComplexMatrix &m);
+ComplexMatrix copyMatrix_asComplex(const RealMatrix &m);
 } // namespace sclMath
