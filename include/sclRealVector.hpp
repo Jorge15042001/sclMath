d8a850b671cc48ea58cc3e5c0d784b26f3868749
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 03:37:17 -0500

    declaration for sclVectors

---
+++ include/sclRealVector.hpp
@@ -0,0 +1,17 @@
+#include "sclRealMatrix.hpp"
+namespace sclMath {
+class RealVector {
+  RealMatrix m_matrix; // a vector is just a (1xn) matrix
+public:
+  RealScalar normSquared() const;
+  RealScalar norm() const;
+
+  RealVector &scale(const RealScalar s);
+
+  std::size_t getSize() const;
+
+  void set(const std::size_t i, const std::size_t j, const RealScalar s);
+  RealScalar get(const std::int64_t i, const std::int64_t j) const;
+};
+std::ostream &operator<<(std::ostream &os, const RealVector &m);
+} // namespace sclMath
