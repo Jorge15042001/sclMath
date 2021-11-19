a19339dabf35fad51fe5933deac9949c81072226
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:33:16 -0500

    separtion of copyMatrix from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -8,12 +8,6 @@
 #include <iostream>
 
 namespace sclMath {
-// copy matrix for sclMath::RealMatrix, sclMath::RealMatrix
-RealMatrix copyMatrix(const RealMatrix &m);
-// copy matrix for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-ComplexMatrix copyMatrix(const ComplexMatrix &m);
-// TODO: copy real matrix as complex matrix, and viseversa
-RealMatrix copyMatrix_asComplex(const ComplexMatrix &m);
 
 // innerProduct
 // ###########################################################################################################
