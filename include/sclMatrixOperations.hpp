5e4c6bb331d9d65c2e1a9ad001187732ac1f9320
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-19 01:35:38 -0500

    separation of innerProduct from sclMatrixOperations header

--- include/sclMatrixOperations.hpp
+++ include/sclMatrixOperations.hpp
@@ -10,17 +10,6 @@
 
 namespace sclMath {
 
-// innerProduct
-// ###########################################################################################################
-// inner Product for sclMath::RealMatrix, sclMath::RealMatrix
-RealScalar innerProduct(const RealMatrix &m1, const RealMatrix &m2);
-// inner Product for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-ComplexScalar innerProduct(const ComplexMatrix &m1, const ComplexMatrix &m2);
-// inner Product for sclMath::ComplexMatrix, sclMath::RealMatrix
-ComplexScalar innerProduct(const ComplexMatrix &m1, const RealMatrix &m2);
-// inner Product for sclMath::RealMatrix, sclMath::ComplexMatrix
-ComplexScalar innerProduct(const RealMatrix &m1, const ComplexMatrix &m2);
-
 // distance
 // ###########################################################################################################
 // distance for sclmath::complexmatrix, sclmath::complexmatrix
