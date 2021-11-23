02e0f95f1e8f37743862219174da7bb3f258aa0a
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-22 23:31:43 -0500

    moving implementation into header file

--- include/sclMatrix/sclMathErrors.hpp
+++ include/sclMatrix/sclMathErrors.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 #include <stdexcept>
 // #include <source_location>
@@ -13,9 +14,19 @@ class sclMathRuntime_error : public std::runtime_error {
 
 public:
   sclMathRuntime_error(const std::string &err_data,
-                       const source_location &err_location);
+                       const source_location &err_location)
+      : std::runtime_error(err_data), errorLocation(err_location) {}
 };
 
-void ASSERT2(bool condition, const std::string &err_data,
-             const source_location &location = source_location::current());
+inline void
+ASSERT2(bool condition, const std::string &err_data,
+        const source_location &location = source_location::current()) {
+#ifdef SCLMATH_DEBUG_BUILD // TODO: check if this works in header only
+                           // implementation
+  if (!condition)
+    throw sclMathRuntime_error(err_data, location);
+#endif
+#ifdef SCLMATH_RELEASE_BUILD
+#endif
+}
 } // namespace sclMathError
