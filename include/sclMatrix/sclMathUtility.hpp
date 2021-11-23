7e0d9525e0a9fb41b53111d80b7c5cc6663110d0
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-22 23:32:40 -0500

    moving implementation in to header file

--- include/sclMatrix/sclMathUtility.hpp
+++ include/sclMatrix/sclMathUtility.hpp
@@ -1,14 +1,31 @@
 #pragma once
-#include "sclComplexMatrix.hpp"
-#include <algorithm>
-#include <cmath>
-#include <iomanip>
-#include <iostream>
-#include <limits>
-#include <type_traits>
+#include "sclMatrix.hpp"
 
 namespace sclMathUtility {
-bool areaAlmostEqual(sclMath::ComplexScalar a, sclMath::ComplexScalar b);
-bool areaAlmostEqual(sclMath::RealScalar a, sclMath::RealScalar b);
-void setUPL(const std::uint64_t);
+// taken from https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+// save comparison for flotating types
+inline std::uint64_t UPL = 6;
+template <class T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+floating_almost_equal(T x, T y, int ulp = UPL) {
+  // the machine epsilon has to be scaled to the magnitude of the values used
+  // and multiplied by the desired precision in ULPs (units in the last place)
+  return std::fabs(x - y) <=
+             std::numeric_limits<T>::epsilon() * std::fabs(x + y) * ulp
+         // unless the result is subnormal
+         || std::fabs(x - y) < std::numeric_limits<T>::min();
+}
+// save comparison between two sclMath::complexScalar
+inline bool areaAlmostEqual(sclMath::ComplexScalar a,
+                            sclMath::ComplexScalar b) {
+  return floating_almost_equal(a.real(), b.real(), UPL) &&
+         floating_almost_equal(a.imag(), b.imag(), UPL);
+}
+// save comparison between two sclMath::Scalar  aka(real number)
+inline bool areaAlmostEqual(sclMath::RealScalar a, sclMath::RealScalar b) {
+  return floating_almost_equal(a, b, UPL);
+}
+
+inline void setUPL(const std::uint64_t s) { UPL = s; }
+
 } // namespace sclMathUtility
