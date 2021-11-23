5a6c69f504fd0869baf975084b00240f18f1f37e
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-22 23:28:58 -0500

    moving implementation into header file

--- include/sclMatrix/sclAngle.hpp
+++ include/sclMatrix/sclAngle.hpp
@@ -1,13 +1,19 @@
+#pragma once
 
-#include "sclComplexMatrix.hpp"
-#include "sclRealMatrix.hpp"
+#include "sclInnerProduct.hpp"
+#include "sclMatrix.hpp"
+
 namespace sclMath {
-
-// angle
-// ###########################################################################################################
+// implementation
 //  angle for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-RealScalar angle(const ComplexMatrix &m1, const ComplexMatrix &m2);
 //  angle for sclMath::RealMatrix, sclMath::RealMatrix
-RealScalar angle(const RealMatrix &m1, const RealMatrix &m2);
+//  angle for sclMath::ComplexMatrix, sclMath::RealMatrix
+//  angle for sclMath::RealMatrix, sclMath::ComplexMatrix
+RealScalar angle(const c_Matrix auto &m1, const c_Matrix auto &m2) {
+  const RealScalar m1Norm = m1.norm();
+  const RealScalar m2Norm = m2.norm();
 
+  const c_Scalar auto innerP = projectToReal(innerProduct(m1, m2));
+  return std::acos(innerP / (m1Norm * m2Norm));
+}
 } // namespace sclMath
