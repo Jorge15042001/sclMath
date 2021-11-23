000a8b91608b9b7a340563cd295a28a1436fac55
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-22 23:08:40 -0500

    moving implementation to header

--- include/sclMatrix/sclMatrixAddition.hpp
+++ include/sclMatrix/sclMatrixAddition.hpp
@@ -1,17 +1,36 @@
-#include "sclComplexMatrix.hpp"
-#include "sclRealMatrix.hpp"
+#pragma once
+
+#include "sclMathErrors.hpp"
+#include "sclMatrix.hpp"
 
 // matrix addition
-// ###########################################################################################################
 // overloading operator + for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator+(const sclMath::ComplexMatrix &m1,
-                                 const sclMath::ComplexMatrix &m2);
 // overloading operator + for sclMath::RealMatrix, sclMath::RealMatrix
-sclMath::RealMatrix operator+(const sclMath::RealMatrix &m1,
-                              const sclMath::RealMatrix &m2);
 // overloading operator + for sclMath::RealMatrix, sclMath::ComplexMatrix
-sclMath::ComplexMatrix operator+(const sclMath::RealMatrix &,
-                                 const sclMath::ComplexMatrix &);
 // overloading operator + for sclMath::ComplexMatrix, sclMath::RealMatrix
-sclMath::ComplexMatrix operator+(const sclMath::ComplexMatrix &m1,
-                                 const sclMath::RealMatrix &m2);
+
+template <sclMath::c_Matrix T_MATRIX1, sclMath::c_Matrix T_MATRIX2>
+sclMath::c_Matrix auto operator+(const T_MATRIX1 &m1, const T_MATRIX2 &m2) {
+
+  typedef typename sclMath::resultType<T_MATRIX1, T_MATRIX2>::type ResultType;
+
+  sclMathError::ASSERT2(
+      m1.getRows() == m2.getRows(),
+      "Addition requires both matrices to have the same shape");
+  sclMathError::ASSERT2(
+      m1.getCols() == m2.getCols(),
+      "Addition requires both matrices to have the same shape");
+
+  const std::size_t rows = m1.getRows();
+  const std::size_t cols = m1.getCols();
+
+  ResultType result(rows, cols);
+
+  for (std::size_t i = 0; i < rows; i++) {
+    for (std::size_t j = 0; j < cols; j++) {
+      const sclMath::c_Scalar auto sum = m1.get(i, j) + m2.get(i, j);
+      result.set(i, j, sum);
+    }
+  }
+  return result;
+}
