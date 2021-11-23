2ce9b32721ccba19034b09ad2997a52bb758a477
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-22 23:30:05 -0500

    moving implementaion in to header file

--- include/sclMatrix/sclInnerProduct.hpp
+++ include/sclMatrix/sclInnerProduct.hpp
@@ -1,16 +1,29 @@
-#include "sclComplexMatrix.hpp"
-#include "sclRealMatrix.hpp"
+#pragma once
+
+#include "sclCopyMatrix.hpp"
+#include "sclMathErrors.hpp"
+#include "sclMatrix.hpp"
+#include "sclMatrixMultiplication.hpp"
+
+#include <type_traits>>
 
 namespace sclMath {
 
 // innerProduct
-// ###########################################################################################################
+
 // inner Product for sclMath::RealMatrix, sclMath::RealMatrix
-RealScalar innerProduct(const RealMatrix &m1, const RealMatrix &m2);
 // inner Product for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-ComplexScalar innerProduct(const ComplexMatrix &m1, const ComplexMatrix &m2);
 // inner Product for sclMath::ComplexMatrix, sclMath::RealMatrix
-ComplexScalar innerProduct(const ComplexMatrix &m1, const RealMatrix &m2);
 // inner Product for sclMath::RealMatrix, sclMath::ComplexMatrix
-ComplexScalar innerProduct(const RealMatrix &m1, const ComplexMatrix &m2);
+c_Scalar auto innerProduct(const c_Matrix auto &m1, const c_Matrix auto &m2) {
+  sclMathError::ASSERT2(
+      m1.getRows() == m2.getRows(),
+      "Inner Product requires both matrices to have the same shape");
+  sclMathError::ASSERT2(
+      m1.getCols() == m2.getCols(),
+      "Inner Product requires both matrices to have the same shape");
+
+  return (copyMatrix(m1).dagger() * m2).trace();
+}
+
 } // namespace sclMath
