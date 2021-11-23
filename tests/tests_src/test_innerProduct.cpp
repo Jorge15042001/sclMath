ab40e8086983a51f1fad3b95328b87e8b98879d8
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-23 00:46:57 -0500

    extend tests for innerProduct

--- tests/tests_src/test_innerProduct.cpp
+++ tests/tests_src/test_innerProduct.cpp
@@ -1,4 +1,5 @@
 #include "sclMatrix/sclInnerProduct.hpp"
+#include "sclMatrix/sclMatrixComparison.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("innerProduct operation", "[innerProduct]") {
@@ -41,8 +42,9 @@ TEST_CASE("innerProduct operation on vector", "[innerProduct]") {
   const sclMath::ComplexScalar result1 = sclMath::innerProduct(m1, m2);
   const sclMath::ComplexScalar result2 = sclMath::innerProduct(m2, m1);
 
+  REQUIRE(result1 == std::conj(result2)); // by mathematical definition
   REQUIRE(result1 == sclMath::ComplexScalar(8, -1));
-  REQUIRE(result2 == sclMath::ComplexScalar(8, -1));
+  REQUIRE(result2 == sclMath::ComplexScalar(8, +1));
 }
 TEST_CASE("innerProduct operation on real matrix", "[innerProduct]") {
 
@@ -86,4 +88,52 @@ TEST_CASE("innerProduct operation on real vector", "[innerProduct]") {
 
   REQUIRE(result1 == 3);
   REQUIRE(result2 == 3);
+}
+TEST_CASE("innerProduct operation on real matrix and ComplexMatrix",
+          "[innerProduct]") {
+
+  sclMath::RealMatrix m1(3, 4);
+  sclMath::ComplexMatrix m2(3, 4);
+
+  m1.set(0, 0, 1);
+  m1.set(0, 2, 0);
+  m1.set(0, 3, 1);
+  m1.set(1, 0, 3);
+  m1.set(2, 0, 1);
+  m1.set(2, 3, 3);
+
+  m2.set(0, 1, 2);
+  m2.set(0, 2, {1, 0});
+  m2.set(0, 3, 3);
+  m2.set(1, 1, 1);
+  m2.set(2, 1, {1, 3});
+  m2.set(2, 2, 1);
+  m2.set(2, 2, {2, 2});
+
+  const sclMath::ComplexScalar result1 = sclMath::innerProduct(m1, m2);
+  const sclMath::ComplexScalar result2 = sclMath::innerProduct(m2, m1);
+
+  REQUIRE(result1 == std::conj(result2)); // by mathematical definition
+  REQUIRE(result1 == 3.);
+  REQUIRE(result2 == 3.);
+}
+TEST_CASE("innerProduct operation on real vector and ComplexVector",
+          "[innerProduct]") {
+  sclMath::RealMatrix m1(3, 1);
+  sclMath::ComplexMatrix m2(3, 1);
+
+  m1.set(0, 0, 1);
+  m1.set(1, 0, 1);
+  m1.set(2, 0, 0);
+
+  m2.set(0, 0, {1, 1});
+  m2.set(1, 0, {2, 3});
+  m2.set(2, 0, {0.9});
+
+  const sclMath::ComplexScalar result1 = sclMath::innerProduct(m1, m2);
+  const sclMath::ComplexScalar result2 = sclMath::innerProduct(m2, m1);
+
+  REQUIRE(result1 == std::conj(result2)); // by mathematical definition
+  REQUIRE(result1 == sclMath::ComplexScalar{3, 4});
+  REQUIRE(result2 == sclMath::ComplexScalar{3, -4});
 }
