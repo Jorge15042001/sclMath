12136b2f4b5ed23e968385ad82594b6b5ebc2e0f
Jorge15042001 <adrivulgarin@hotmail.com>
2021-11-27 02:24:59 -0500

    change api to copy matrix

--- include/sclMatrix/sclCopyMatrix.hpp
+++ include/sclMatrix/sclCopyMatrix.hpp
@@ -4,43 +4,15 @@
 
 namespace sclMath {
 
-inline RealMatrix copyMatrix(const RealMatrix &m) {
-  const std::size_t rows = m.getRows();
-  const std::size_t cols = m.getCols();
-
-  // TODO: matrix should do its own copy
-  // TODO: if not method to get m_data should exists
-  RealMatrix mCopy(rows, cols);
-  for (std::size_t i = 0; i < rows; i++) {
-    for (std::size_t j = 0; j < cols; j++) {
-      mCopy.set(i, j, m.get(i, j));
-    }
-  }
-  return mCopy;
-}
 // copy matrix for sclMath::ComplexMatrix, sclMath::ComplexMatrix
-inline ComplexMatrix copyMatrix(const ComplexMatrix &m) {
+// copy matrix for sclMath::RealMatrix, sclMath::RealMatrix
+
+template <c_Matrix T_MATRIX, c_Matrix T_OUTPUT = T_MATRIX>
+T_OUTPUT copyMatrix(const T_MATRIX &m) {
   const std::size_t rows = m.getRows();
   const std::size_t cols = m.getCols();
 
-  // TODO: matrix should do its own copy
-  // TODO: if not method to get m_data should exists
-  ComplexMatrix mCopy(rows, cols);
-  for (std::size_t i = 0; i < rows; i++) {
-    for (std::size_t j = 0; j < cols; j++) {
-      mCopy.set(i, j, m.get(i, j));
-    }
-  }
-  return mCopy;
-}
-// TODO: copy real matrix as complex matrix, and viseversa
-inline ComplexMatrix copyMatrix_asComplex(const RealMatrix &m) {
-  const std::size_t rows = m.getRows();
-  const std::size_t cols = m.getCols();
-
-  // TODO: matrix should do its own copy
-  // TODO: if not method to get m_data should exists
-  ComplexMatrix mCopy(rows, cols);
+  T_OUTPUT mCopy(rows, cols);
   for (std::size_t i = 0; i < rows; i++) {
     for (std::size_t j = 0; j < cols; j++) {
       mCopy.set(i, j, m.get(i, j));
