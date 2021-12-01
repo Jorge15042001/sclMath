e4626c64cb4510a300e0f2c910e9c0871c39beaf
Jorge15042001 <adrivulgarin@hotmail.com>
2021-12-01 00:24:42 -0500

    using std::equal

--- include/sclMatrix/sclMatrixComparison.hpp
+++ include/sclMatrix/sclMatrixComparison.hpp
@@ -2,27 +2,28 @@
 
 #include "sclMathUtility.hpp"
 #include "sclMatrix.hpp"
+
+#include <algorithm>>
 // == operator for matrices
 
 // overloading operator == for ComplexMatrix
 // overloading operator == for realmatrices
 // overloading operator == for sclMath::RealMatrix and sclMath::ComplexMatrix
 // overloading operator == for sclMath::ComplexMatrix and sclMath::RealMatrix
-bool operator==(const sclMath::c_Matrix auto &m1,
-                const sclMath::c_Matrix auto &m2) {
+template <sclMath::c_Scalar TS1, sclMath::c_Scalar TS2>
+bool operator==(const sclMath::Matrix<TS1> &m1,
+                const sclMath::Matrix<TS2> &m2) {
+
   if (m1.getRows() != m2.getRows())
     return false;
   if (m1.getCols() != m2.getCols())
     return false;
 
-  const std::size_t rows = m1.getRows();
-  const std::size_t cols = m1.getCols();
+  const std::vector<TS1> &m1_data = m1.getDataVector();
+  const std::vector<TS2> &m2_data = m2.getDataVector();
 
-  for (std::size_t i = 0; i < rows; i++) {
-    for (std::size_t j = 0; j < cols; j++) {
-      if (!sclMathUtility::areaAlmostEqual(m1.get(i, j), m2.get(i, j)))
-        return false;
-    }
-  }
-  return true;
+  return std::equal(m1_data.begin(), m1_data.end(), m2_data.begin(),
+                    [](const TS1 s1, const TS2 s2) {
+                      return sclMathUtility::areaAlmostEqual(s1, s2);
+                    });
 }
