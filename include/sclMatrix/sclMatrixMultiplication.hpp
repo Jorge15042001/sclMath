991551c34d559f5a34c2c3aaab67604cb6af3eab
Jorge15042001 <adrivulgarin@hotmail.com>
2021-12-10 18:10:08 -0500

    multiplication by zero matrix now supported

--- include/sclMatrix/sclMatrixMultiplication.hpp
+++ include/sclMatrix/sclMatrixMultiplication.hpp
@@ -10,34 +10,38 @@
 // overloading operator * for sclMath::ComplexMatrix, sclMath::RealMatrix
 
 /* template <sclMath::c_Scalar T_SCALAR1, sclMath::c_Scalar T_SCALAR2> */
-sclMath::c_FullMatrix auto operator*(const sclMath::c_FullMatrix auto &m1,
-                                 const sclMath::c_FullMatrix auto &m2) {
+sclMath::c_AnyMatrix auto operator*(const sclMath::c_AnyMatrix auto &m1,
+                                    const sclMath::c_AnyMatrix auto &m2) {
+  // type deduction code
+  typedef std::remove_cvref_t<decltype(m1)> m1_type;
+  typedef std::remove_cvref_t<decltype(m2)> m2_type;
 
-  /* typedef typename sclMath::ScalarResultType<>::type ResultType; */
-  typedef typename sclMath::MatrixResultType<
-      std::remove_cvref_t<decltype(m1)>,
-      std::remove_cvref_t<decltype(m2)>>::type MatrixType;
+  typedef typename sclMath::MatrixResultType<m1_type, m2_type>::type MatrixType;
   typedef typename sclMath::MatrixScalarType<MatrixType>::type ScalarType;
 
   sclMathError::ASSERT2(
       m1.getCols() == m2.getRows(),
       "sclMath::matrix multiplication is not defined, incompatible shapes");
+  // if any of the matrices is a ZeroMatrix
+  if constexpr (sclMath::anyIsZeroMatrix<m1_type, m2_type>) {
+    return sclMath::ZeroMatrix(m1.getRows(), m2.getCols());
+  } else {
+    const std::size_t n = m1.getCols();
+    const std::size_t rows = m1.getRows();
+    const std::size_t cols = m2.getCols();
 
-  const std::size_t n = m1.getCols();
-  const std::size_t rows = m1.getRows();
-  const std::size_t cols = m2.getCols();
+    MatrixType result(rows, cols);
 
-  MatrixType result(rows, cols);
-
-  for (std::size_t i = 0; i < rows; i++) {
-    for (std::size_t j = 0; j < cols; j++) {
-      ScalarType sum = 0.0;
-      for (std::size_t h = 0; h < n; h++) {
-        sum += m1.get(i, h) * m2.get(h, j);
+    for (std::size_t i = 0; i < rows; i++) {
+      for (std::size_t j = 0; j < cols; j++) {
+        ScalarType sum = 0.0;
+        for (std::size_t h = 0; h < n; h++) {
+          sum += m1.get(i, h) * m2.get(h, j);
+        }
+        result.set(i, j, sum);
       }
-      result.set(i, j, sum);
     }
-  }
 
-  return result;
+    return result;
+  }
 }
