5b4a087db71d6090828f5ecadfe2bdf0b3900cb5
Jorge15042001 <adrivulgarin@hotmail.com>
2021-12-17 17:46:52 -0500

    overloads for matrix addition with zero matrix

--- include/sclMatrix/sclMatrixAddition.hpp
+++ include/sclMatrix/sclMatrixAddition.hpp
@@ -27,11 +27,41 @@ sclMath::c_FullMatrix auto operator+(const sclMath::Matrix<T_SCALAR1> &m1,
   const std::size_t rows = m1.getRows();
   const std::size_t cols = m1.getCols();
 
-  std::vector<T_OUT> result_data(rows * cols);
+  std::vector<ScalarType> result_data(rows * cols);
 
   std::transform(m1.getDataVector().begin(), m1.getDataVector().end(),
                  m2.getDataVector().begin(), result_data.begin(),
                  std::plus<>());
 
-  return sclMath::Matrix<T_OUT>(rows, cols, std::move(result_data));
+  return MatrixType(rows, cols, std::move(result_data));
+}
+inline sclMath::ZeroMatrix operator+(const sclMath::ZeroMatrix &m1,
+                                     const sclMath::ZeroMatrix &m2) {
+  sclMathError::ASSERT2(
+      m1.getRows() == m2.getRows(),
+      "Addition requires both matrices to have the same shape");
+  sclMathError::ASSERT2(
+      m1.getCols() == m2.getCols(),
+      "Addition requires both matrices to have the same shape");
+  return sclMath::ZeroMatrix(m1.getRows(), m2.getCols());
+}
+sclMath::c_FullMatrix auto operator+(const sclMath::ZeroMatrix &m1,
+                                     const sclMath::c_FullMatrix auto &m2) {
+  sclMathError::ASSERT2(
+      m1.getRows() == m2.getRows(),
+      "Addition requires both matrices to have the same shape");
+  sclMathError::ASSERT2(
+      m1.getCols() == m2.getCols(),
+      "Addition requires both matrices to have the same shape");
+  return sclMath::copyMatrix(m2);
+}
+sclMath::c_FullMatrix auto operator+(const sclMath::c_FullMatrix auto &m1,
+                                     const sclMath::ZeroMatrix &m2) {
+  sclMathError::ASSERT2(
+      m1.getRows() == m2.getRows(),
+      "Addition requires both matrices to have the same shape");
+  sclMathError::ASSERT2(
+      m1.getCols() == m2.getCols(),
+      "Addition requires both matrices to have the same shape");
+  return sclMath::copyMatrix(m1);
 }
