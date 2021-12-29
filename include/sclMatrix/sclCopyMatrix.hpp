0cb9e01ee115ebdc665bea1b04b2bb9a91e18f1d
Jorge15042001 <adrivulgarin@hotmail.com>
2021-12-28 23:22:33 -0500

    copyZeroMatrix

--- include/sclMatrix/sclCopyMatrix.hpp
+++ include/sclMatrix/sclCopyMatrix.hpp
@@ -10,21 +10,25 @@ namespace sclMath {
 
 // INVALID copy matrix from sclMath::ComplexMatrix, sclMath::RealMatrix
 
-template <c_FullMatrix T_MATRIX, c_FullMatrix T_OUTPUT = T_MATRIX>
+template <c_AnyMatrix T_MATRIX, c_AnyMatrix T_OUTPUT = T_MATRIX>
 T_OUTPUT copyMatrix(const T_MATRIX &m) {
+  if constexpr (isZeroMatrix<T_MATRIX>) {
+    return T_OUTPUT(m);
+  } else {
+    typedef typename MatrixScalarType<T_MATRIX>::type ScalarType_input;
+    typedef typename MatrixScalarType<T_OUTPUT>::type ScalarType_output;
 
-  typedef typename MatrixScalarType<T_MATRIX>::type ScalarType_input;
-  typedef typename MatrixScalarType<T_OUTPUT>::type ScalarType_output;
+    const std::vector<ScalarType_input> &data = m.getDataVector();
+    std::vector<ScalarType_output> dataCopy;
+    // get enough space to store the data
+    dataCopy.reserve(data.size());
+    // fill the copy with data from the origicil vector
+    dataCopy.insert(dataCopy.end(), data.begin(), data.end());
 
-  const std::vector<ScalarType_input> &data = m.getDataVector();
-  std::vector<ScalarType_output> dataCopy;
-  // get enough space to store the data
-  dataCopy.reserve(data.size());
-  // fill the copy with data from the origicil vector
-  dataCopy.insert(dataCopy.end(), data.begin(), data.end());
+    T_OUTPUT mCopy(m.getRows(), m.getCols(), std::move(dataCopy));
 
-  T_OUTPUT mCopy(m.getRows(), m.getCols(), std::move(dataCopy));
-
-  return mCopy;
+    return mCopy;
+  }
 }
+
 } // namespace sclMath
