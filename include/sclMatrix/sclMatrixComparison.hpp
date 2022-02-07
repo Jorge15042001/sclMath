787a575b46b134ce692dd38806efd06c78e76a81
Jorge15042001 <adrivulgarin@hotmail.com>
2022-02-06 23:16:56 -0500

    generalizing comparison

--- include/sclMatrix/sclMatrixComparison.hpp
+++ include/sclMatrix/sclMatrixComparison.hpp
@@ -10,17 +10,27 @@
 // overloading operator == for realmatrices
 // overloading operator == for sclMath::RealMatrix and sclMath::ComplexMatrix
 // overloading operator == for sclMath::ComplexMatrix and sclMath::RealMatrix
-template <sclMath::c_Scalar TS1, sclMath::c_Scalar TS2>
-bool operator==(const sclMath::Matrix<TS1> &m1,
-                const sclMath::Matrix<TS2> &m2) {
+bool operator==(const sclMath::c_AnyMatrix auto &m1,
+                const sclMath::c_AnyMatrix auto &m2) {
+
+  // TODO: introduce abstraction sclMath::isNotFullMatri and use for branching
+  // to different behaviour
+  typedef std::remove_cvref_t<decltype(m1)> m1_type;
+  typedef std::remove_cvref_t<decltype(m2)> m2_type;
+  // check if the matrices are the same type
+
+  // check if any two matices are equal
+  typedef typename sclMath::MatrixScalarType<m1_type>::type ScalarType1;
+  typedef typename sclMath::MatrixScalarType<m2_type>::type ScalarType2;
 
   if (m1.getRows() != m2.getRows())
     return false;
   if (m1.getCols() != m2.getCols())
     return false;
-
+  // check if all entries are equal
   return std::equal(m1.getDataVector().begin(), m1.getDataVector().end(),
-                    m2.getDataVector().begin(), [](const TS1 s1, const TS2 s2) {
+                    m2.getDataVector().begin(),
+                    [](const ScalarType1 s1, const ScalarType2 s2) {
                       return sclMathUtility::areaAlmostEqual(s1, s2);
                     });
 }
